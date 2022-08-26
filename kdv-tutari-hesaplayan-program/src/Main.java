98ac27684d70e69299825a1f954b9f629d2e4a57
Begüm Arıcı <aricibegum@gmail.com>
2022-08-26 18:25:01 +0300

    Main eklendi

--- kdv-tutari-hesaplayan-program/src/Main.java
+++ kdv-tutari-hesaplayan-program/src/Main.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        double tutar, kdvTutari, total;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ücret tutarını giriniz: ");
+        tutar = input.nextDouble();
+
+        if (0 < tutar && tutar < 1000) {
+            kdvTutari = tutar*0.18;
+            total = tutar + kdvTutari;
+
+            System.out.println("KDV'siz tutar: " + tutar);
+            System.out.println("KDV oranı: %18");
+            System.out.println("KDV tutarı: " + kdvTutari);
+            System.out.println("KDV'li fiyat: " + total);
+
+
+        } else if (tutar >= 1000) {
+            kdvTutari = tutar*0.08;
+            total = tutar + kdvTutari;
+            System.out.println("KDV'siz tutar: " + tutar);
+            System.out.println("KDV oranı: %8");
+            System.out.println("KDV tutarı: " + kdvTutari);
+            System.out.println("KDV'li fiyat: " + total);
+
+        }
+    }
 }
