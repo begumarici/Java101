f78fba615a9050636c934731cf301c40d9b5a5ab
Begüm Arıcı <aricibegum@gmail.com>
2022-08-26 19:58:58 +0300

    guncellendi

--- taksimetre-hesaplayan-program/src/Main.java
+++ taksimetre-hesaplayan-program/src/Main.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        double km, perKm = 2.20, total, startPrice = 10;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Mesafeyi KM cinsinden giriniz: ");
+        km = input.nextDouble();
+
+        total = (km * perKm);
+        total += startPrice;
+
+        total = (total < 20 ) ? 20 : total;
+        System.out.println("Toplam tutar: " + total);
+
+    }
 }
