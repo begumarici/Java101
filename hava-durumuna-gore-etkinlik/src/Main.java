3977fb579feaec759f666c2269ee2d9c9d0d3074
Begüm Arıcı <aricibegum@gmail.com>
2022-08-27 17:18:09 +0300

    yeni program eklendi

---
+++ hava-durumuna-gore-etkinlik/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int heat;
+
+        System.out.println("Sıcaklık giriniz: ");
+        heat = input.nextInt();
+
+        if (heat < 5){
+            System.out.println("Kayak yapabilirsiniz.");
+        } else if (heat >=5 && heat <=15){
+            System.out.println("Sinemaya gidebilirsiniz. ");
+        } else if (heat >15 && heat <=25){
+            System.out.println("Piknik yapabilirsiniz. ");
+        } else {
+            System.out.println("Yüzmeye gidebilirsiniz. ");
+        }
+
+    }
+}
