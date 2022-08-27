dfa4dc81e5ec811dfc9080d33e7e6755b729f9c9
Begüm Arıcı <aricibegum@gmail.com>
2022-08-27 10:53:09 +0300

    yeni program eklendi

---
+++ dairenin-diliminin-alanini-hesaplayan-program/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double r, a, alan, pi = 3.14;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Dairenin yarıçapını giriniz: ");
+        r = scan.nextDouble();
+        System.out.println("Dairenin merkez açısının ölçüsünü giriniz: ");
+        a = scan.nextDouble();
+
+        alan = (pi*(r*r)*a)/360;
+
+        System.out.println("Dairenin alanı: " + alan);
+
+    }
+}
